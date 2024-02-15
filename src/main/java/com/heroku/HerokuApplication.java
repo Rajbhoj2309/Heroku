28a1eb46913a35ceb2df8e0ade3e8d6570aa3c2a
Rajbhoj2309 <kiranrajbhoj89@gmail.com>
2024-02-15 16:26:18 +0530

    production file added. Closes #3

--- src/main/java/com/heroku/HerokuApplication.java
+++ src/main/java/com/heroku/HerokuApplication.java
@@ -2,10 +2,23 @@ package com.heroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 //@EnableJpaRepositories(basePackages = "com.heroku.repository")
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
+//
+/*
+ * if we want add multiple properties the we can used
+ * 
+ * @PropertySources({
+ * 
+ * @PropertySource(
+ * "classpath:application-${spring.profiles.active:default}.properties") })
+ */
+
 public class HerokuApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuApplication.class, args);
